1cd8a8906ae7c713373717dbc718a372486c3a7c
LaurenDarrimon <laurendarrimon@gmail.com>
2022-03-09 22:40:40 -0800

    create clinet side graphQL queries that will be passed to React pages and components

---
+++ client/src/utils/queries.js
@@ -0,0 +1,35 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_USERS = gql`
+    query allUsers {
+        users {
+            _id
+            githubUser
+            name
+            avatar
+            blog
+            location
+            bio
+        }
+    }
+`
+
+export const QUERY_SINGLE_USER = gql`
+    query singleUser($githubUser: String!) {
+        user(githubUser: $githubUser) {
+            _id
+            githubUser
+            name
+            avatar
+            blog
+            location
+            bio
+        }
+    }
+`
+
+export const QUERY_USER_PROJECTS = gql`
+    query userProjects($githubUser: String!) {
+        project
+    }
+`
