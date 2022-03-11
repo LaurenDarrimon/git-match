43b83dd2d19d82ddafe6c4bd8e699614fade578e
plotinusspascual <plotinusspascual@gmail.com>
2022-03-11 00:33:30 -0800

    able to use githubAPI to fetch user starred repo and render them on page

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,28 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useMutation } from '@apollo/client';
 import { ADD_PROJECT} from '../utils/mutations';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
 import  API  from '../utils/fetch'
+import ProjectList from '../components/ProjectList';
+import { resultKeyNameFromField } from '@apollo/client/utilities';
 
 const Dashboard = () => {
-    const [toggle, setToggle] = useState({
-        state: 'user'   
-    });
+    // const [toggle, setToggle] = useState({
+    //     state: 'user'   
+    // });
     
-    const [addProject, { error, data }] = useMutation(ADD_PROJECT);
+    // const [addProject, { error, data }] = useMutation(ADD_PROJECT);
+    const [projectName, setProjectName] = useState("");
+    const [projectDescription, setProjectDescription] = useState("");
+    const [repoLink, setRepoLink] = useState("");
 
-    const projectFetcher= async () => {
+    useEffect(() => {
+      API.fetchStarred(Auth.getProfile().data.githubUser)
+        .then(async (response) => {
+          const data = await response;
+          console.log(data[0].name);
+          // project name
+          if(data && data[0].name){
+            setProjectName(data[0].name);
+          }
+          // project description
+          if(data && data[0].description){
+            setProjectDescription(data[0].description);
+          }
+          // repo link
+          if(data && data[0].repo_link){
+            setRepoLink(data[0].repo_link);
+          }
+        });
+    }, []);
 
-        const fetchedProjects =  await API.fetchStarred(Auth.getProfile().data.githubUser);
-        return fetchedProjects
-    }
+    // function loadProjects() {
+    //     API.fetchStarred(Auth.getProfile().data.githubUser)
+    //       .then(response => response.json())
+    //       .then( data => document.body.append());
+    // }
+    // console.log(loadProjects());
 
-    const projectPromise = projectFetcher();
-    projectPromise.then(function(result) {
-        const projectArray = result;
-        console.log('projectarray',projectArray);
-    })
+
     // const CreateProject = projectArray.map((project,index) => {
     //     const { data } = addProject({
     //         variables: {
@@ -36,7 +58,11 @@ const Dashboard = () => {
         <div>
             <h5>{Auth.getProfile().data.githubUser} Dashboard</h5>
             <h4>Projects/Users Matches</h4>
+            <h1>{projectName}</h1>
+            <h2>{projectDescription}</h2>
+            <h2>{repoLink}</h2>
         </div>
+        
     )
 };
 
--- client/src/utils/fetch.js
+++ client/src/utils/fetch.js
@@ -9,9 +9,9 @@ const API = {
       axios.get(`https://api.github.com/users/${user}`)
         .then((res) => {
           //Populate the database
-          console.log('res', res);
+          // console.log('res', res);
           const user = res.data;
-          console.log('user',user);
+          // console.log('user',user);
           const results = {
             name: user.name,
             avatar: user.avatar_url,
@@ -31,25 +31,25 @@ const API = {
     return new Promise((resolve, reject) => {
     axios.get(`https://api.github.com/users/${user}/starred`)
       .then((res) => {
+        // console.log(res)
         const projects = res.data;
-        // console.log('projects',projects);
+        // console.log('projects', projects);
         const results = projects?.map((project) => {
            return {
             name: project.name,
             description: project.description,
             repo_link: project.html_url
-
           }
         });
-        console.log('results',results);
+        // console.log(results);
         resolve(results)
       }).catch((err) => reject(err));
     })
   },
+  
 
   //fetch a repo's programming languagesfrom GitHUb API
   fetchLanguage: (user, repo) => {
-
     axios.get(`https://api.github.com/repos/${user}/${repo}/languages`)
       .then((res) => {
         return res;
