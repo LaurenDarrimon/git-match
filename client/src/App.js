be7ba9efb32077d0fddbbb43f8e17729ea76559b
LaurenDarrimon <laurendarrimon@gmail.com>
2022-03-15 14:11:05 -0700

    add Abut page with Formspree contact form integration

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,50 @@
-import React from 'react';
+import React from "react";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Match from "./pages/Match";
+import About from "./pages/About";
+import Form from "./components/Form";
+import Project from "./components/Project";
+import Profile from "./pages/Profile";
+import CreateProject from "./pages/CreateProject";
+import NotFound from "./pages/NotFound";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-import Home from './pages/Home';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import Match from './pages/Match'
-import Project from './components/Project';
-import Profile from './pages/Profile';
-import CreateProject from './pages/CreateProject';
-import NotFound from './pages/NotFound';
-import Nav from './components/Nav';
-import Header from './components/Header';
-import Footer from './components/Footer';
-
-import './index.css';
+import "./index.css";
 
 import "./assets/fonts/LEMONMILK-Regular.woff2";
 import "./assets/fonts/LEMONMILK-Regular.woff";
 import "./assets/fonts/LEMONMILK-Medium.woff2";
 import "./assets/fonts/LEMONMILK-Medium.woff";
 
-
-
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -54,7 +53,9 @@ const client = new ApolloClient({
   // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
-  onError: (e) => { console.log(e) }
+  onError: (e) => {
+    console.log(e);
+  },
 });
 
 function App() {
@@ -65,34 +66,15 @@ function App() {
           <Header />
           <div className="container">
             <Routes>
-              <Route
-                path="/"
-                element={<Home />}
-              />
-              <Route
-                path="/login"
-                element={<Login />}
-              />
-              <Route
-                path="/signup"
-                element={<Signup />}
-              />
-              <Route
-                path="/profiles/:githubUser"
-                element={<Profile />}
-              />
-              <Route
-                path="/profiles/"
-                element={<Profile />}
-              />
-              <Route
-                path="/me"
-                element={<Dashboard />}
-              />
-              <Route
-                path="/matches/:githubUser"
-                element={<Match />}
-              />
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/profiles/:githubUser" element={<Profile />} />
+              <Route path="/profiles/" element={<Profile />} />
+              <Route path="/me" element={<Dashboard />} />
+              <Route path="/about" element={<About />} />
+
+              <Route path="/matches/:githubUser" element={<Match />} />
 
               {/*
               <Route 
---
+++ client/src/pages/About.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import Form from "../components/Form";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+export default function About() {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+  return (
+    <section className="row section-top" id="contact">
+      <div className="col-md-5 d-flex flex-column" id="contact-info">
+          <br>
+          </br>
+          <br>
+          </br>
+        <h4>Contact the GitMatch Team</h4>
+        <h6>With questions, comments, and details about contributing to this project!</h6>
+      </div>
+
+      <div className="col-md-5">
+        <h2
+          className="reach-out"
+          data-aos="fade-right"
+          data-aos-offset="300"
+          data-aos-easing="ease-in-sine"
+        >
+          REACH <span className="serif">OUT</span>
+        </h2>
+
+        <div>
+          <Form />
+        </div>
+      </div>
+    </section>
+  );
+}
