aa5c9bc8138df84ec88672b1c8a92ea698d50677
David Chou <davidchou2009@gmail.com>
2022-03-15 11:47:03 -0700

    Add additional buttons/links to Nav bar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { QUERY_USERS } from '../utils/queries';
+import { useQuery, useMutation } from "@apollo/client";
 
 import Auth from "../utils/auth";
 import logo from "../assets/images/git-match-logo.png";
@@ -10,6 +12,14 @@ const Header = () => {
     event.preventDefault();
     Auth.logout();
   };
+  // const userData = useQuery(QUERY_USERS);
+  // console.log(userData);
+  // console.log('random user');
+  // console.log(userData.data.users[0].githubUser);
+  // const randIndex = Math.floor(Math.random() * userData.data.users.length)
+  // const randUser = userData.data.users[randIndex].githubUser;
+  // .users[Math.floor(Math.random()*userData.data.users.length)].githubUser
+
   return (
     <header className="dark text-light mb-4 py-3 align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
@@ -28,6 +38,17 @@ const Header = () => {
               <button className="btn btn-lg m-2 gradient" onClick={logout}>
                 Logout
               </button>
+              <Link to="/profiles"><button className="btn btn-lg m-2 gradient">
+                My Profile
+              </button></Link>
+              <Link to="/"><button className="btn btn-lg m-2 gradient">
+                Home
+              </button></Link>
+              {/* {userData.data ?
+                <Link to={`/profiles/${userData.data.users[randUser].githubUser}`}><button className="btn btn-lg m-2 gradient">
+                  Start Matching
+                </button></Link> : <></>
+              } */}
             </>
           ) : (
             <></>
